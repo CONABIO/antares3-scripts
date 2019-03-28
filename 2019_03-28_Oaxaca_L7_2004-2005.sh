bf646029215350812f3ba631092460be8b9f8219
Erick Palacios Moreno <palmoreck@gmail.com>
2019-03-28 15:47:35 -0600

    adding specifications of amount of ram for each worker and scheduler

--- 2019_03-28_Oaxaca_L7_2004-2005.sh
+++ 2019_03-28_Oaxaca_L7_2004-2005.sh
@@ -1,4 +1,4 @@
-#Usamos db: test_no_gaps
+#Usamos db: test_no_gaps para .datacube.conf y test_from_s3_to_s3_2 para .antares
 
 #bucket test-landsat-l7
 #6 r4.xlarge instances
@@ -31,17 +31,19 @@ Command execution is done in 261.692054271698 seconds.
 
 antares segment --algorithm bis -n oax_seg_50km_no_gaps_0405 -p l7_oax_50km_no_gaps_recipe_0405 -r Oaxaca -b ndvi_mean --datasource landsat7 --year 2005 -extra t=30 s=0.5 c=0.7 -sc /shared_volume/scheduler.json
 
-2019-03-20 17:26:59,352 - madmex.management.base - base execute: Command execution is done in 924.7343854904175 seconds.
+Command execution is done in 924.7343854904175 seconds.
 
 antares model_fit -model rf -p l7_oax_50km_no_gaps_recipe_0405 -t oax_31 --region Oaxaca -sp mean --name rf_l7_oax_0405_50km_no_gaps --sample 1 --remove-outliers -extra n_estimators=60 -sc /shared_volume/scheduler.json
 
 Command execution is done in 159.22286462783813 seconds.
 
-#4 gb each dask worker, 17 dask-workers
+#scheduler 4gb, 4 gb each dask worker, 17 dask-workers
 
 antares model_predict_object -p l7_oax_50km_no_gaps_recipe_0405 -m rf_l7_oax_0405_50km_no_gaps -s oax_seg_50km_no_gaps_0405 -r Oaxaca --name land_cover_rf_l7_oax_0405_50km_no_gaps -sc /shared_volume/scheduler.json
 
 Command execution is done in 363.85774970054626 seconds.
+
+#scheduler 10gb, 2gb each dask worker
 
 antares db_to_raster -n land_cover_rf_l7_oax_0405_50km_no_gaps -region Oaxaca -f land_cover_rf_l7_oax_0405_50km_no_gaps_new_db_to_raster.tif --resolution 30 -sc /shared_volume/scheduler.json
 
