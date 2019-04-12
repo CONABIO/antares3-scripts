e7e4a0c50d58cdb42872b0077906dbb01a0918c4
Erick Palacios Moreno <palmoreck@gmail.com>
2019-04-12 13:51:59 -0500

    new script for mexico lc 95 year

---
+++ 2019-04-12_mexico_lc_landsat_1995.sh
@@ -0,0 +1,46 @@
+#buckets that will hold data ingestion process:
+
+#srtm:
+datacube-srtm-mexico-s3
+
+#datacube-ls5-7-espa-mexico-s3
+
+#database to use in both .antares and .datacube.conf 
+
+#antares_datacube 
+
+#on server: aurora
+
+#volume in shared_volume to use that will hold antares prepare_metadata results:
+
+/shared_volume/tasks/2019/mexico_landsat_lc
+
+#0)init datacube: (check .datacube.conf properly configured)
+
+datacube -v system init --no-init-users
+
+#1)ingest:
+
+#? r4.xlarge instances
+
+# scheduler 4 gb, 16 workers with 5 gb each
+
+antares prepare_metadata --path linea_base/L5 --bucket conabio-s3-oregon --dataset_name landsat_espa --outfile metadata_mex_l5.yaml -sc /shared_volume/scheduler.json
+
+#just to have an idea: #count number of datasets that will be ingested:
+
+grep id: /shared_volume/tasks/2019/mexico_landsat_lc/metadata_mex_l5.yaml|wc -l
+
+datacube -v product add ~/.config/madmex/indexing/ls5_espa_scenes.yaml
+
+datacube -v dataset add metadata_mex_l5.yaml
+
+# before running ingest add the bucket name to the yaml file that will store ingestion results. Bucket: datacube-ls5-7-espa-mexico-s3
+
+#also check queue-size....and modify properly this will launch 2000 tasks
+
+datacube -v ingest --queue-size 2000 -c ~/.config/madmex/ingestion/ls5_espa_mexico.yaml --executor distributed <ip_scheduler>:8786
+
+
+
+
