70b60b2788b328f4b151061eae883780e53c919d
Erick Palacios Moreno <palmoreck@gmail.com>
2019-05-06 17:17:36 -0500

    adding time for model fit

--- 2019-04-12_mexico_lc_Landsat5.sh
+++ 2019-04-12_mexico_lc_Landsat5.sh
@@ -216,8 +216,9 @@ predict_rf_veracruz_L5_9596_comp
 #We detect some change of classes between contiguous states of Mexico, therefore we decided fit a model to all Mexico:
 
 #5 r4.2xlarge instances
-#scheduler 57 gb, 34 workers with 2gb each
+#scheduler 10 gb, 29 workers with 2gb each
 
 antares model_fit -model rf -p recipe_mex_L5_9596 -t bits_nacional -lat 14 33 -long -119 -84 --name model_rf_mexico_L5_9596 --sample 1 --remove-outliers -extra n_estimators=60 -sc /shared_volume/scheduler.json
 
 
+Command execution is done in 4129.5213894844055 seconds.
