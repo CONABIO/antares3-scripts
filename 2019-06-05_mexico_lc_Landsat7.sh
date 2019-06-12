d28453c8e12f7851d3b558038a96a6a2f5f5d78a
ixime <ixmezach@googlemail.com>
2019-06-12 14:17:40 -0500

    add recipe

--- 2019-06-05_mexico_lc_Landsat7.sh
+++ 2019-06-05_mexico_lc_Landsat7.sh
@@ -16,6 +16,8 @@ datacube-ls5-7-espa-mexico-s3
 # scheduler 25 gb, 95 workers with 5 gb each
 
 antares prepare_metadata --path linea_base/L7 --bucket conabio-s3-oregon --dataset_name landsat_espa --outfile metadata_mex_l7_1415.yaml --pattern ".*LE07[0-9]{6}(2014|2015).*" -sc /shared_volume/scheduler.json
+antares prepare_metadata --path linea_base/L7_complement/2014_NOGAPS --bucket conabio-s3-oregon --dataset_name landsat_espa --outfile metadata_mex_l7_14_comp.yaml -sc /shared_volume/scheduler.json
+antares prepare_metadata --path linea_base/L7_complement/2015_NOGAPS --bucket conabio-s3-oregon --dataset_name landsat_espa --outfile metadata_mex_l7_15_comp.yaml -sc /shared_volume/scheduler.json
 
 #just to have an idea: #count number of datasets that will be ingested:
 
@@ -25,6 +27,8 @@ grep id: /shared_volume/tasks/2019/mexico_landsat_lc/metadata_mex_l7_1415.yaml|w
 datacube -v product add ~/.config/madmex/indexing/ls7_espa_scenes.yaml
 
 datacube -v dataset add metadata_mex_l7_1415.yaml
+datacube -v dataset add metadata_mex_l7_14_comp.yaml
+datacube -v dataset add metadata_mex_l7_15_comp.yaml
 
 # before running ingest add the bucket name to the yaml file that will store ingestion results. 
 #Bucket: datacube-ls5-7-espa-mexico-s3
@@ -39,3 +43,15 @@ datacube -v ingest -c ~/.config/madmex/ingestion/ls7_espa_mexico.yaml --executor
 
 ######################(end) 1)ingest
 
+######################2)recipe:
+
+#2014 and 2015 will be used
+#will use lat long coordinates of bounding box for Mexico instead of region flag
+
+#30 r4.2xlarge instances. Scheduler 10 gb and 29 workers 57 gb
+
+antares apply_recipe -recipe landsat_madmex_003 -b 2014-01-01 -e 2015-12-31 -lat 14 33 -long -119 -84 --name recipe_mex_L5_1415 -sc /shared_volume/scheduler.json
+
+#Aprox  hrs
+
+######################(end)2)recipe
