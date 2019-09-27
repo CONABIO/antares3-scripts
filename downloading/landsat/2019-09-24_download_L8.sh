d781057925a9665967822e7007fb2daed6a1c52f
Roberto Martínez <rmartinez@conabio.gob.mx>
2019-09-27 15:17:43 -0500

    commands for create order and download order

--- downloading/landsat/2019-09-24_download_L8.sh
+++ downloading/landsat/2019-09-24_download_L8.sh
@@ -1,4 +1,12 @@
 # mexico_contorno contorno es una geometría en la DB para consultas más rápidas
 # su equivalente en shapefile esta en /LUSTRE/MADMEX/shapefiles/mexico_contorno_simple/mex_simple.shp
 
-antares create_order --region 'mexico_contorno'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 30 --landsat 8 --no-order
+# petición 
+antares create_order --region 'mexico_contorno'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 30 --landsat 8 
+antares create_order --region 'mexico_contorno'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 10 --landsat 7 
+
+# descarga
+antares download_order --order_ids espa-conabio.madmex@gmail.com-09252019-180347-664
+
+# por ejecutar: 
+antares download_order --order_ids espa-conabio.madmex@gmail.com-09252019-185334-420
