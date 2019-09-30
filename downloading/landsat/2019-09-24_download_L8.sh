fe8c879ede01018f5ec1e7e14152de522690b2b3
Roberto Martínez <rmartinez@conabio.gob.mx>
2019-09-30 16:09:17 -0500

    Fixing country geometry name

--- downloading/landsat/2019-09-24_download_L8.sh
+++ downloading/landsat/2019-09-24_download_L8.sh
@@ -2,8 +2,8 @@
 # su equivalente en shapefile esta en /LUSTRE/MADMEX/shapefiles/mexico_contorno_simple/mex_simple.shp
 
 # petición 
-antares create_order --region 'mexico_simple'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 30 --landsat 8 
-antares create_order --region 'mexico_simple'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 10 --landsat 7 
+antares create_order --region 'mex_simple'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 30 --landsat 8 
+antares create_order --region 'mex_simple'  --start-date '2014-01-01' --end-date '2015-12-31' --max-cloud-cover 10 --landsat 7 
 
 # descarga
 antares download_order --order_ids espa-conabio.madmex@gmail.com-09252019-180347-664
