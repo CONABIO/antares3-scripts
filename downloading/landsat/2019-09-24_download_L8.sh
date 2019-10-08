39a2a39217e62ef027e9b3fbd157aba03c23ddc9
Erick Palacios Moreno <palmoreck@gmail.com>
2019-10-08 12:29:49 -0500

    adding download of L8 2017-2018

--- downloading/landsat/2019-09-24_download_L8.sh
+++ downloading/landsat/2019-09-24_download_L8.sh
@@ -15,6 +15,10 @@ antares create_order --region 'mex_simple'  --start-date '2014-01-01' --end-date
 antares create_order --region 'mex_simple'  --start-date '2017-01-01' --end-date '2018-12-31' --max-cloud-cover 30 --landsat 8
 # order id: espa-conabio.madmex@gmail.com-10012019-115837-913 , total escenas: 4843
 
+4. Descarga Landsat 8 2017-2018
+Cambiar ruta de TEMP_DIR en `.antares` con: /LUSTRE/MADMEX/imagenes/landsat/L8/2017_2018
+Crear ruta /LUSTRE/MADMEX/imagenes/landsat/L8/2017_2018
+antares download_order -order_ids espa-conabio.madmex@gmail.com-10012019-115837-913
 
 
 
