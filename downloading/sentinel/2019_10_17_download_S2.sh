a9abea3ea09f0185f30645ad907a5220e70ba04b
Erick Palacios Moreno <palmoreck@gmail.com>
2019-10-17 12:40:21 -0500

    adding some notes

--- downloading/sentinel/2019_10_17_download_S2.sh
+++ downloading/sentinel/2019_10_17_download_S2.sh
@@ -1,10 +1,12 @@
 # El archivo geojson son es un contorno simple de México que abarca todas sus islas, se localiza en /LUSTRE/MADMEX/shapefiles/mexico_geojson_simple/mex_full.geojson
 # estos comandos descargan escenas Sentinel-2 2018 desde 2018-09-30 hasta 2018-12-31
 
+#next line to count number of scenes and size
 sentinelsat -u conabio_1 -p madmex... -g mex_full.geojson --sentinel 2 --url https://scihub.copernicus.eu/dhus --producttype S2MSI1C -s 20181001 -e 20181231 --cloud 25 --footprints --path /LUSTRE/MADMEX/imagenes/sentinel2_escenas/2018/
 7001 scenes found with a total size of 3242.47 GB
 
 # Ruta de descarga: /LUSTRE/MADMEX/imagenes/sentinel2_escenas/2018/
+#execute next lines in: /LUSTRE/MADMEX/shapefiles/mexico_geojson_simple
 sentinelsat -u conabio_1 -p madmex... -g mex_full.geojson --sentinel 2 --url https://scihub.copernicus.eu/dhus --producttype S2MSI1C -s 20181001 -e 20181008 --cloud 25 -d --path /LUSTRE/MADMEX/imagenes/sentinel2_escenas/2018/
 sentinelsat -u conabio_1 -p madmex... -g mex_full.geojson --sentinel 2 --url https://scihub.copernicus.eu/dhus --producttype S2MSI1C -s 20181009 -e 20181017 --cloud 25 -d --path /LUSTRE/MADMEX/imagenes/sentinel2_escenas/2018/
 sentinelsat -u conabio_1 -p madmex... -g mex_full.geojson --sentinel 2 --url https://scihub.copernicus.eu/apihub --producttype S2MSI1C -s 20181018 -e 20181026 --cloud 25 -d --path /LUSTRE/MADMEX/imagenes/sentinel2_escenas/2018/
